eb80e37aa8bfccf12c8c86ef4bb4bba53459739d
Jackson Barreto <jackson.barreto@gmail.com>
2023-07-22 22:38:49 +0200

    session tranfer ok

--- src/main/java/com/example/projeto2/RegisterDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.projeto2;
-
-import com.example.projeto2.models.UserRole;
-
-public record RegisterDTO(String username, String password, UserRole role) {
-}
---
+++ src/main/java/com/example/projeto2/models/RegisterDTO.java
@@ -0,0 +1,61 @@
+package com.example.projeto2;
+
+public class RegisterDTO {
+
+    private String name;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String userRole;
+
+    public RegisterDTO() {
+    }
+    public RegisterDTO(String name, String username, String password, String phoneNumber, String userRole) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.userRole = userRole;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
+
