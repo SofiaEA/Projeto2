a10d4229534579ed79a53f27dc28d82e9a7cc0dd
Sofia Amorim <79472325+SofiaEA@users.noreply.github.com>
2023-07-23 18:39:10 +0100

    perfil

--- src/main/java/com/example/projeto2/controllers/AppController.java
+++ src/main/java/com/example/projeto2/controllers/AppController.java
@@ -1,13 +1,30 @@
 package com.example.projeto2.controllers;
 
+import com.example.projeto2.repository.UserRepository;
+import com.example.projeto2.services.UserDetailsServiceImpl;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/app")
 public class AppController {
+
+    @Resource(name = "userService")
+    private UserDetailsServiceImpl userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/home")
     public ModelAndView home() {
@@ -18,8 +35,13 @@ public class AppController {
 
     @GetMapping("/perfil")
     public ModelAndView perfil() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("perfil");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<UserDetails> user = userRepository.findByUsername(username);
+
+        ModelAndView modelAndView = new ModelAndView("perfil");
+        modelAndView.addObject("user", user);
+
         return modelAndView;
     }
+
 }
